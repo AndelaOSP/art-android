5a0a24545e9d0dd64fc827a72ea9e5164bcc3a03
godwin gitonga <godwingitonga@godwins-MacBook-Pro.local>
2018-03-09 16:20:18 +0300

    feat(check-in): add end to end tests - add checkin activity tests - set up mock server

---
+++ app/src/androidTest/java/com/andela/art/main/MainActivityTest.java
@@ -0,0 +1,49 @@
+package com.andela.art.main;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.filters.LargeTest;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.andela.art.R;
+import com.andela.art.checkin.CheckInActivity;
+
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+
+    @Rule
+    public IntentsTestRule<MainActivity> intentsTestRule = new IntentsTestRule<>(MainActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void testCheckInButtonClickOpensCheckInActivity()
+    {
+        onView(withId(R.id.buttonCheckIn)).perform(click());
+        intended(hasComponent(CheckInActivity.class.getName()));
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
