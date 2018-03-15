cf2f3f287fef9f2eb96db881dbddc8892e781291
eric-elem <eric.elem@andela.com>
2018-03-15 09:46:13 +0300

    Lock screen orientation to portrait

--- app/src/main/java/com/andela/art/root/App.java
+++ app/src/main/java/com/andela/art/root/App.java
@@ -1,6 +1,10 @@
 package com.andela.art.root;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.util.Log;
 
 import com.andela.art.login.LoginModule;
 
@@ -20,6 +24,43 @@ public class App extends Application {
                 .loginModule(new LoginModule())
                 .build();
 
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(Activity activity, Bundle bundle) {
+                activity.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                Log.i("tests", "2. setting activities orientation");
+            }
+
+            @Override
+            public void onActivityStarted(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(Activity activity, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(Activity activity) {
+
+            }
+        });
     }
 
     /**
