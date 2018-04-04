1facfe4a33b25e252d7251a74efa1ce2f07d310c
kyadondo Timothy <chadwalt04@gmail.com>
2018-04-04 10:15:47 +0300

    #156222795 Create dashboard for security guards (#27)
    * Add profile image to the dashboard
    
    * Add fullname, email, and button
    
    * feat(Security dashboard): modify the layout file
    
    - Add fonts
    - Change the margins of views in layouts
    
    * Add profile image to the dashboard
    
    * Add fullname, email, and button
    
    * feat(Security dashboard): modify the layout file
    
    - Add fonts
    - Change the margins of views in layouts
    
    * Add app tools and fix avenir_book.otf duplication
    
    * Add id to the toolbar and align it on top of the imageview
    
    * Add id to the toolbar and align it on top of the imageview
    
    * fix height on the actionbar, set the default color for the text
    
    * feat(dashboard activity): add tests for the UI views
    
    * feat(review): add all values to resource files
    
    - move all figures to dimens
    - move all color codes to colors
    - modify tests to work with hidden TextViews

--- app/src/androidTest/java/com/andela/art/securitydashboard/SecurityDashboardTest.java
+++ app/src/androidTest/java/com/andela/art/securitydashboard/SecurityDashboardTest.java
@@ -1,12 +1,17 @@
 package com.andela.art.securitydashboard;
 
+import android.support.test.espresso.UiController;
+import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.intent.Intents;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.TextView;
 
 import com.andela.art.R;
 import com.andela.art.settings.SettingsActivity;
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,6 +26,8 @@ import static android.support.test.espresso.intent.matcher.IntentMatchers.hasCom
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static org.hamcrest.CoreMatchers.allOf;
 
 /**
  * Tests for the security dashboard.
@@ -63,5 +70,50 @@ public class SecurityDashboardTest {
 
         intended(hasComponent(SettingsActivity.class.getName()));
         Intents.release();
+    }
+
+    /**
+     * Test the dashboard activity that it loads and displays all the views.
+     */
+    @Test
+    public void dashboardViewsAreRenderedInDashboardActivity() {
+        onView(withId(R.id.profile_picture)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.full_name)).perform(setTextInTextView("Full name"));
+        onView(withId(R.id.full_name)).check(matches(isDisplayed()));
+        onView(withId(R.id.full_name)).check(matches(withText("Full name")));
+
+        onView(withId(R.id.email_address)).perform(setTextInTextView("test_email@domain.com"));
+        onView(withId(R.id.email_address)).check(matches(isDisplayed()));
+        onView(withId(R.id.email_address)).check(matches(withText("test_email@domain.com")));
+
+        onView(withId(R.id.check_serial)).check(matches(isDisplayed()));
+        onView(withId(R.id.check_serial)).check(matches(withText(R.string.check_serial)));
+    }
+
+    /**
+     * Custom Matchers method to help set the text of a TextView while testing using Espresso.
+     *
+     * @param value The string to set on the TextView
+     * @return ViewAction
+     */
+    public static ViewAction setTextInTextView(final String value) {
+        return new ViewAction() {
+            @SuppressWarnings("unchecked")
+            @Override
+            public Matcher<View> getConstraints() {
+                return allOf(isAssignableFrom(TextView.class));
+            }
+
+            @Override
+            public void perform(UiController uiController, View view) {
+                ((TextView) view).setText(value);
+            }
+
+            @Override
+            public String getDescription() {
+                return "replace text";
+            }
+        };
     }
 }
