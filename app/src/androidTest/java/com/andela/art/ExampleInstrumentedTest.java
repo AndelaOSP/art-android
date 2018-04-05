e4177d91344a381e995e502ed477c50c068452ce
Zacharia <zacmwangi94@gmail.com>
2018-04-05 17:32:29 +0300

    Fix failing tests

--- app/src/androidTest/java/com/andela/art/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/andela/art/ExampleInstrumentedTest.java
@@ -27,6 +27,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.andela.art", appContext.getPackageName());
+        assertEquals("com.andela.art.mock", appContext.getPackageName());
     }
 }
