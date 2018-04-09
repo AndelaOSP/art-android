701b61db160746487119647cf06190ed3d787b8e
Zacharia <zacmwangi94@gmail.com>
2018-04-09 13:10:43 +0300

    Fix tests

--- app/src/androidTest/java/com/andela/art/securitydashboard/SecurityDashboardActivityTest.java
+++ app/src/androidTest/java/com/andela/art/securitydashboard/SecurityDashboardActivityTest.java
@@ -20,7 +20,6 @@ import okhttp3.mockwebserver.MockResponse;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.intent.Intents.intended;
