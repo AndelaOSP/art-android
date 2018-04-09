0d6578d96cd034de25235f6a6c015aab622df43f
Zacharia <zacmwangi94@gmail.com>
2018-04-09 15:10:53 +0300

    fix merge conflicts

--- app/src/main/java/com/andela/art/common/ApplicationComponent.java
+++ app/src/main/java/com/andela/art/common/ApplicationComponent.java
@@ -28,12 +28,8 @@ public interface ApplicationComponent {
          * @param artApplication application instance to be used by builder
          * @return Builder
          */
-<<<<<<< HEAD
         @BindsInstance
         Builder application(ArtApplication artApplication);
-=======
-        @BindsInstance Builder application(ArtApplication artApplication);
->>>>>>> develop
 
         /**
          * Application component builder.
