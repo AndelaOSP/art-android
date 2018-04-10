c5a2ab1c38d52d9837f02a5cb4b966bf0988f0e1
chadwalt <chadwalt04@gmail.com>
2018-04-10 14:42:18 +0300

    Add tests for report a problem

---
+++ app/src/androidTest/java/com/andela/art/settings/ReportProblemTest.java
@@ -0,0 +1,51 @@
+package com.andela.art.settings;
+
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.andela.art.R;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * Tests for the ReportProblem activity.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ReportProblemTest {
+
+    @Rule
+    public ActivityTestRule<ReportProblemActivity> mActivityRule =
+            new ActivityTestRule<ReportProblemActivity>(ReportProblemActivity.class);
+
+    /**
+     * Test the close button.
+     */
+    @Test
+    public void clickCloseReportButton() {
+        onView(withId(R.id.close_report_button)).check(matches(isDisplayed()));
+
+        onView(withId(R.id.close_report_button)).perform(click());
+        onView(withText("Settings")).check(matches(isDisplayed()));
+    }
+
+    /**
+     * Test the rendering of the report a problem views.
+     */
+    @Test
+    public void renderReportProblewViews() {
+        onView(withText("Report a Problem")).check(matches(isDisplayed()));
+        onView(withText("Report a Problem")).check(matches(isDisplayed()));
+
+    }
+}
