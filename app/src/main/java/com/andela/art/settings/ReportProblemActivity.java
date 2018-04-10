389619ac3b34ec48875eec7392d2f736b5973243
chadwalt <chadwalt04@gmail.com>
2018-04-10 16:06:22 +0300

    Add data binding

--- app/src/main/java/com/andela/art/settings/ReportProblemActivity.java
+++ app/src/main/java/com/andela/art/settings/ReportProblemActivity.java
@@ -1,12 +1,12 @@
 package com.andela.art.settings;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.ImageView;
 
 import com.andela.art.R;
+import com.andela.art.databinding.ActivityReportProblemBinding;
 
 /**
  * Report issues about the application.
@@ -21,24 +21,20 @@ public class ReportProblemActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_report_problem);
+        ActivityReportProblemBinding reportBinding = DataBindingUtil.setContentView(
+                this, R.layout.activity_report_problem
+        );
+        setSupportActionBar(reportBinding.reportToolbar);
 
-        Toolbar toolbar = findViewById(R.id.report_toolbar);
-        setSupportActionBar(toolbar);
-
-        // Remove the default toolbar title.
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-
-        ImageView closeReportView = findViewById(R.id.close_report_button);
-        closeReportView.setOnClickListener(closeReportListener);
     }
 
     /**
-     * Move back to the previous activity.
+     * Cancel reporting a problem and move back to the previous activity.
+     *
+     * @param view The view that calls this method which is an ImageView
      */
-    private final View.OnClickListener closeReportListener = new View.OnClickListener() {
-        public void onClick(View v) {
-            finish();
-        }
-    };
+    public void cancelReportSending(View view) {
+        finish();
+    }
+
 }
