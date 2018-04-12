7695f301bd63ee45f0563a447b87c5b168219b2e
Muhwezi Allan <allan.muhwezi@andela.com>
2018-04-12 16:33:57 +0300

    feat(Feedback Activity): create Feedback Activity and its layout xml

---
+++ app/src/main/java/com/andela/art/feedback/FeedbackActivity.java
@@ -0,0 +1,18 @@
+package com.andela.art.feedback;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.andela.art.R;
+
+/**
+ * This activity handles the submitting of feedback from a user.
+ */
+public class FeedbackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+    }
+}
