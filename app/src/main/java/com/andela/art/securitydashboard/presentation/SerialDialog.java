216cff0d00ef8b1434027e3dec4e135b14635901
godwin gitonga <godwingitonga@godwins-MacBook-Pro.local>
2018-04-17 15:05:47 +0300

    feat(CheckIn): Add check in activity mock data [Delivers #155121723]

--- app/src/main/java/com/andela/art/securitydashboard/presentation/SerialDialog.java
+++ app/src/main/java/com/andela/art/securitydashboard/presentation/SerialDialog.java
@@ -1,6 +1,7 @@
 package com.andela.art.securitydashboard.presentation;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
@@ -8,7 +9,10 @@ import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 
 import com.andela.art.R;
+import com.andela.art.checkin.CheckInActivity;
 import com.andela.art.databinding.SerialDialogBinding;
+import com.andela.art.models.Asset;
+import com.andela.art.models.AssignedTo;
 
 /**
  * Created by zack on 3/27/18.
@@ -41,6 +45,7 @@ public class SerialDialog extends AppCompatDialogFragment {
         dialogBinding.submit.setOnClickListener(view -> {
             String serial = dialogBinding.serialEditText.getText().toString();
             ((SecurityDashboardActivity) getActivity()).onConfirmClicked(serial);
+            goToCheckIn();
             dismiss();
         });
 
@@ -51,5 +56,43 @@ public class SerialDialog extends AppCompatDialogFragment {
         return new AlertDialog.Builder(getActivity())
                 .setView(dialogBinding.getRoot())
                 .create();
+    }
+
+    /**
+     * Get to Check in activity.
+     */
+    public void goToCheckIn() {
+        Intent intent = new Intent(getActivity(), CheckInActivity.class);
+
+        AssignedTo assignedTo = new AssignedTo();
+        assignedTo.setCohort(18);
+        assignedTo.setDateJoined("20-12-2017");
+        assignedTo.setEmail("mudge.fudge@mail.com");
+        assignedTo.setFirstName("mudge");
+        assignedTo.setLastName("Fudge");
+        assignedTo.setFullName("Mudge Fudge");
+        assignedTo.setPicture(null);
+        assignedTo.setId(1);
+        assignedTo.setSlackHandle("");
+        assignedTo.setLastLogin("");
+        assignedTo.setLastModified("");
+
+
+        Asset asset = new Asset();
+        asset.setId(1);
+        asset.setAllocationStatus("Checkin");
+        asset.setAssignedTo(assignedTo);
+        asset.setCheckinStatus("Checkout");
+        asset.setCreatedAt("12-09-2018");
+        asset.setItemCode("ER34521");
+        asset.setLastModified("");
+        asset.setModelNumber("");
+        asset.setSerialNumber("CRT5647Y");
+
+        Bundle bundle = new Bundle();
+        bundle.putSerializable("asset", asset);
+        intent.putExtras(bundle);
+
+        startActivity(intent);
     }
 }
