b5b6fca074de2fc73b403421783d22d22956e1cd
MadgeKinyanjui <35771033+MadgeKinyanjui@users.noreply.github.com>
2018-05-03 16:14:09 +0300

    ft(andela-user-dashboard):User dashboard Activity (#59)

---
+++ app/src/main/java/com/andela/art/userdashboard/presentation/UserDashBoardFragment.java
@@ -0,0 +1,84 @@
+package com.andela.art.userdashboard.presentation;
+
+import android.databinding.DataBindingUtil;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.andela.art.R;
+import com.andela.art.databinding.FragmentUserDashboardBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.squareup.picasso.Picasso;
+
+/**
+ * Displays the user dashboard screen.
+ */
+public class UserDashBoardFragment extends Fragment implements UserDashBoardView {
+    private static final String ARG_ACCOUNT = "google_account";
+    private GoogleSignInAccount mGoogleSignInAccount;
+    FragmentUserDashboardBinding binding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mGoogleSignInAccount = getArguments()
+                .getParcelable(ARG_ACCOUNT);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+         binding = DataBindingUtil.inflate(
+                inflater, R.layout.fragment_user_dashboard, container, false);
+
+        String fullName = mGoogleSignInAccount.getDisplayName();
+        binding.name.setText(fullName);
+
+        String email = mGoogleSignInAccount.getEmail();
+        binding.emailAddress.setText(email);
+
+        Uri photoUrl = mGoogleSignInAccount.getPhotoUrl();
+        loadResizedImage(photoUrl);
+
+        return binding.getRoot();
+    }
+
+    /**
+     * Instances of this fragment to have an account object be passed.
+     *
+     * @param account - GoogleSignInAccount
+     * @return fragment
+     */
+    public static UserDashBoardFragment newInstance(GoogleSignInAccount account) {
+        Bundle arguments = new Bundle();
+        arguments.putParcelable(ARG_ACCOUNT, account);
+
+        UserDashBoardFragment fragment = new UserDashBoardFragment();
+        fragment.setArguments(arguments);
+        return fragment;
+    }
+
+    @Override
+    public void loadResizedImage(Uri url) {
+
+        if (url == null) {
+
+            Picasso.with(getActivity())
+                    .load(R.drawable.photo)
+                    .fit()
+                    .centerCrop()
+                    .into(binding.ivPhoto);
+        } else {
+            Picasso.with(getActivity())
+                    .load(url)
+                    .fit()
+                    .centerCrop()
+                    .into(binding.ivPhoto);
+        }
+    }
+}
