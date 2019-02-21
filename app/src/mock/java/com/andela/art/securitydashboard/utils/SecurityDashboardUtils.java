abf3ebc3e3af4d3e1463696c44856c199c1156ac
Kalela <philipkalela@gmail.com>
2019-02-21 13:47:01 +0300

    bug(failing tests): Fix failing instrumentation tests
    - Switch dummy json data to match new schema
    
    [Delivers: 164133975]

--- app/src/mock/java/com/andela/art/securitydashboard/utils/SecurityDashboardUtils.java
+++ app/src/mock/java/com/andela/art/securitydashboard/utils/SecurityDashboardUtils.java
@@ -1,8 +1,9 @@
 package com.andela.art.securitydashboard.utils;
 
 
+import android.widget.Toast;
+
 import com.andela.art.api.UserAssetResponse;
-import com.andela.art.models.Asignee;
 import com.andela.art.models.Asset;
 import com.andela.art.securitydashboard.presentation.SerialView;
 
@@ -10,9 +11,10 @@ import com.andela.art.securitydashboard.presentation.SerialView;
  * Mock SecurityDashboardUtils class.
  */
 public class SecurityDashboardUtils {
+    private static final String TAG = "Check";
     SerialView view;
     Asset asset;
-    Asignee asignee;
+    UserAssetResponse responseAsset;
 
     /**
      * constructor.
@@ -21,49 +23,25 @@ public class SecurityDashboardUtils {
      */
     public SecurityDashboardUtils(UserAssetResponse asset, SerialView view) {
         this.view = view;
-        this.asset = new Asset();
+        this.responseAsset = asset;
     }
 
     /**
      * initiate Checkin intent.
      */
     public void goToCheckin() {
-        setDummyData();
-        view.handleCheckinIntent(asset);
+        if (responseAsset.getCount() == 0) {
+            view.handleToast("The asset is not available.",
+                    Toast.LENGTH_LONG, false);
+        } else {
+            asset = responseAsset.getAssets().get(0);
+            if (asset.getAssignee() == null) {
+                view.handleToast("Asset not assigned.",
+                        Toast.LENGTH_LONG, false);
+            } else {
+                view.handleCheckinIntent(asset);
+            }
+        }
     }
 
-    /**
-     * Set asset dummy data.
-     *
-     */
-    private void setDummyData() {
-        //Asignee
-        asignee = new Asignee();
-        asignee.setCohort(18);
-        asignee.setSlackHandle(null);
-        asignee.setDateJoined("2018-04-19T08:48:54.859461Z");
-        asignee.setFirstName("Margaret");
-        asignee.setFullName("Margaret Kinyanjui");
-        asignee.setEmail("margaret.kinyanjui@andela.com");
-        asignee.setId(21);
-        asignee.setLastLogin(null);
-        asignee.setLastModified("2018-04-19T08:48:55.061424Z");
-        asignee.setLastName("Kinyanjui");
-        asignee.setPhoneNumber(null);
-        asignee.setPicture(null);
-
-
-        //Asset
-        asset.setSerialNumber("serial1");
-        asset.setCheckinStatus("checked_in");
-        asset.setAssignedTo(asignee);
-        asset.setAllocationStatus("Available");
-        asset.setAssetType("Extension Cord");
-        asset.setCreatedAt("2018-03-22T14:47:26.977310Z");
-        asset.setId(3);
-        asset.setItemCode(null);
-        asset.setLastModified("2018-05-10T09:43:09.950324Z");
-        asset.setModelNumber(null);
-
-    }
 }
